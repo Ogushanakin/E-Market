7431acc3c6d5ad26e65f2e6ad21e1a99f512b8fd
Oğuzhan Akın <55364051+Ogushanakin@users.noreply.github.com>
2024-08-05 19:01:19 +0300

    initial commit

---
+++ E-Market/E-Market/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  E-Market
+//
+//  Created by Oğuzhan Akın on 5.08.2024.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
